9e0b6ad4a22328a6b2eb56e472d6413b120bbf0d
0n1dev <all_mind@naver.com>
2021-02-26 16:43:59 +0900

    자료구조 배열

---
+++ Array/src/DS_Array.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class DS_Array {
+    public static void main(String[] args) {
+        /**
+         * 배열 초기화 방법 1
+         * */
+        int[] arr1 = new int[3];
+
+        arr1[0] = 1;
+        arr1[1] = 2;
+        arr1[2] = 3;
+
+        /**
+         * 배열 초기화 방법 2
+         * */
+        int[] arr2 = {1, 2, 3};
+    }
+}
